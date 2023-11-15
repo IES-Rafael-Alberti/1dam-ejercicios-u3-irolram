0ec8835695a457b7a43a88eb0d3cb8f3aa66d873
irolram <rrivan@iesrafaelalberti.es>
2023-11-15 19:15:53 +0100

    hasta4

--- src/ej3_03.py
+++ src/ej3_03.py
@@ -5,10 +5,24 @@ por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura>
 cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas 
 introducidas por el usuario."""
 
+def ingresar_nota(asignaturas):
+
+    notas = []
 
 
+    for asignatura in asignaturas:
+        nota = float(input(f"Qué nota ha sacado en {asignatura}: "))
+        notas.append(nota)
+    return notas
+
+def mostrar_nota(asignaturas, notas):
+    for i in range(len(asignaturas)):
+        print(f"En {asignaturas[i]} has sacado {notas[i]} ")
 
 def main():
+    asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+    notas = ingresar_nota(asignaturas)
+    mostrar_nota(asignaturas, notas)
 
 
 if __name__ == "__main__":

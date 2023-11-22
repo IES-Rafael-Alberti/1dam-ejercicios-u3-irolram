f29ec6ef6d496100f387668726706677d4060430
irolram <rrivan@iesrafaelalberti.es>
2023-11-22 03:23:03 +0100

    hasta el 6

--- src/ej3.2.diccionarios.py/dicc_03.py
+++ src/ej3.2.diccionarios.py/dicc_03.py
@@ -6,3 +6,32 @@ Manzana	0.80
 Pera	0.85
 Naranja	0.70"""
 
+def preguntar_usuario_fruta():
+    fruta = input("Qué fruta quieres?: ")
+    kilos = input(f"Cuantos kilos de {fruta} quieres")
+    return fruta
+
+    
+
+
+def precio_fruta(fruta):
+    Plátano = "Plátano"
+    Manzana = "Manzana"
+    Pera = "Pera"
+    Naranja = "Naranja"
+
+    precio = {Plátano: "1.35", Manzana: "0.80", Pera: "0.85", Naranja: "0.70"}
+
+    return precio
+
+def main():
+
+    precio = precio_fruta()
+    preguntar_usuario_fruta()
+
+
+
+
+    if __name__ == "__main__":
+        main()
+
